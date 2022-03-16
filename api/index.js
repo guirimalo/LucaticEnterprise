0f6966eb86368425c5428d8cb4d9e791b6bd2e3b
miguelbayoDAW <miguelpuerta16@gmail.com>
2022-03-16 19:17:16 +0100

    Test Jest

--- api/index.js
+++ api/index.js
@@ -13,16 +13,19 @@ app.use(express.static(__dirname + "/public"));
 // Leer fichero
 const fs = require('fs');
 
-let rawdata = fs.readFileSync('./empleados.json');
-let data = JSON.parse(rawdata);
+
 
 //app.get('/', function(req, res) {
   //res.send("../web/home.html");
 //});
 
 function getAllEmpleados() {
+  let rawdata = fs.readFileSync('./empleados.json');
+  let data = JSON.parse(rawdata);
   return data;
 }
+
+
 
 app.get('/empleados', function(req, res) {
   res.setHeader('Content-Type', 'application/json');
@@ -30,3 +33,5 @@ app.get('/empleados', function(req, res) {
 });
 
 app.listen(8080);
+
+module.exports = getAllEmpleados;
